99c61c824d60fd5bd1f545d74dcae79c7e27e206
wanwusangzhi1992 <wanwusangzhi1992@163.com>
2018-11-17 21:45:57 +0800

    deploy

---
+++ precache-manifest.58ea1fd7bab14efd1b0f4e5524859bdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ddb9cfc500ed391d89ff",
+    "url": "/js/chunk-vendors.27822585.js"
+  },
+  {
+    "revision": "afff7257b36c419399df",
+    "url": "/js/app.70da9212.js"
+  },
+  {
+    "revision": "444e27eade4f3960f66f",
+    "url": "/js/about.8b907bfe.js"
+  },
+  {
+    "revision": "c21c5d7eb91bc64c8d0db56919dc9e60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "afff7257b36c419399df",
+    "url": "/css/app.22862076.css"
+  }
+];
